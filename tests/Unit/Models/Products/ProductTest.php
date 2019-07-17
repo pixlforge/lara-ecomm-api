473e82de139390a42be6a09cb4f3d63eb311c086
Pixlforge <celien@pixlforge.ch>
2019-07-17 09:21:40 +0200

    Simple products

---
+++ tests/Unit/Models/Products/ProductTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit\Models\Products;
+
+use Tests\TestCase;
+use App\Models\Product;
+
+class ProductTest extends TestCase
+{
+    /** @test */
+    public function it_uses_the_slug_for_the_route_key_name()
+    {
+        $product = new Product();
+
+        $this->assertEquals($product->getRouteKeyName(), 'slug');
+    }
+}
